d5b1dc16295b62d491e5649c8dfa6fede41ac907
Laszlo Zsidek <laszlo.zsidek@dmgmori-heitec.hu>
2021-10-16 09:20:08 +0200

    index: -navigation to edit employee -delete button

--- src/main/java/hu/webuni/hr/lzsidek/web/EmployeeTLController.java
+++ src/main/java/hu/webuni/hr/lzsidek/web/EmployeeTLController.java
@@ -5,11 +5,11 @@ import hu.webuni.hr.lzsidek.service.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -17,18 +17,18 @@ public class EmployeeTLController {
 
     @Autowired
     SalaryService salaryService;
-    private List<Employee> employees = new ArrayList<>();
+    private Map<Long, Employee> employees = new HashMap<>();
 
     {
-        employees.add(new Employee(1L, "A.Stark", "CEO", LocalDateTime.of(2019,4,6,0,0)));
-        employees.add(new Employee(2L, "N.Romanoff", "CIO", LocalDateTime.of(2016,10,6,0,0)));
-        employees.add(new Employee(3L, "S.Rogers", "COO", LocalDateTime.of(2016,2,2,0,0)));
-        employees.add(new Employee(4L, "B.Banner", "CIT", LocalDateTime.of(2010,2,2,0,0)));
+        employees.put(1L, new Employee(1L, "A.Stark", "CEO", LocalDateTime.of(2019,4,6,0,0)));
+        employees.put(2L, new Employee(2L, "N.Romanoff", "CIO", LocalDateTime.of(2016,10,6,0,0)));
+        employees.put(3L, new Employee(3L, "S.Rogers", "COO", LocalDateTime.of(2016,2,2,0,0)));
+        employees.put(4L, new Employee(4L, "B.Banner", "CIT", LocalDateTime.of(2010,2,2,0,0)));
     }
 
-    @GetMapping("/")
+    @GetMapping("/employees")
     public String home(Map<String, Object> model) {
-        for (Employee employee : employees) {
+        for (Employee employee : employees.values()) {
             salaryService.setSalaryOfNewEmployee(employee);
         }
         model.put("employees", employees);
@@ -36,9 +36,15 @@ public class EmployeeTLController {
         return "index";
     }
 
-    @PostMapping("/")
+    @PostMapping("/employees")
     public String addEmployee(Employee employee) {
-        employees.add(employee);
-        return "redirect:";
+        employees.put(employee.getId(), employee);
+        return "redirect:employees";
+    }
+
+    @GetMapping("/employees/{id}")
+    public String editEmployee(@PathVariable long id, Map<String, Object> model) {
+        model.put("edited_employee", employees.get(id));
+        return "employee";
     }
 }
