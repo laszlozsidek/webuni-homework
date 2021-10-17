ea3aab31a754fb265585d0ad328412882d34d25e
Laszlo Zsidek <laszlo.zsidek@dmgmori-heitec.hu>
2021-10-17 12:51:29 +0200

    getPayRisePercent OK

---
+++ src/main/java/hu/webuni/hr/lzsidek/web/EmployeeServiceController.java
@@ -0,0 +1,22 @@
+package hu.webuni.hr.lzsidek.web;
+
+import hu.webuni.hr.lzsidek.model.Employee;
+import hu.webuni.hr.lzsidek.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employeeService")
+public class EmployeeServiceController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public int getPayRisePercent(@RequestBody Employee employee) {
+        return employeeService.getPayRaisePercent(employee);
+    }
+}
